9d4356e4291552700586069b0d3357c75ad701ef
Jake Qi <astromist18@gmail.com>
2022-12-02 19:46:00 -0500

    Added users to db and routes.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
-from db import *
+from db import db
 from flask import Flask, request
+import users_dao
+import datetime
+
 import json
 import os
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "journal.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -23,8 +26,144 @@ def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
 
+def extract_token(request):
+    """
+    Helper function that extracts the token from the header of a request
+    """
+    auth_header = request.headers.get("Authorization")
+    if auth_header is None:
+        return False, failure_response("Missing authorization header", 400)
+
+    bearer_token = auth_header.replace("Bearer ", "").strip()
+    if bearer_token is None or not bearer_token:
+        return False, failure_response("Invalid authorization header", 400)
+
+    return True, bearer_token
+
+
 @app.route("/")
 def base_endpoint():
     return os.environ.get("NETID") + " was here!"
 
 
+@app.route("/register/", methods=["POST"])
+def register_account():
+    """
+    Endpoint for registering a new user
+    """
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+
+    if email is None or password is None:
+        return failure_response("Missing email or password", 400)
+
+    success, user = users_dao.create_user(email, password)
+
+    if not success:
+        return failure_response("User already exists", 400)
+
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
+
+
+@app.route("/login/", methods=["POST"])
+def login():
+    """
+    Endpoint for logging in a user
+    """
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+
+    if email is None or password is None:
+        return failure_response("Missing email or password!", 400)
+
+    success, user = users_dao.verify_credentials(email, password)
+
+    if not success: 
+        return failure_response("Incorrect email or password", 401)
+
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
+
+
+@app.route("/session/", methods=["POST"])
+def update_session():
+    """
+    Endpoint for updating a user's session
+    """
+    success, update_token = extract_token(request)
+
+    if not success:
+        return failure_response("Could not extract update token", 400)
+
+    success_user, user = users_dao.renew_session(update_token)
+
+    if not success_user:
+        return failure_response("Invalid update token")
+
+    return success_response({
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token
+    })
+
+
+@app.route("/secret/", methods=["GET"])
+def secret_message():
+    """
+    Endpoint for verifying a session token and returning a secret message
+
+    In your project, you will use the same logic for any endpoint that needs 
+    authentication
+    """
+    success, session_token = extract_token(request)
+
+    if not success:
+        return failure_response("Could not extract session token", 400)
+
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token", 400)
+
+    # handle logic of the endpoint
+    # e.g. editing their profile
+
+    return success_response({"message": "You have successfully implemented sessions!"})
+
+
+@app.route("/logout/", methods=["POST"])
+def logout():
+    """
+    Endpoint for logging out a user
+    """
+    success, session_token = extract_token(request)
+
+    if not success:
+        return failure_response("Could not extract session token", 400)
+
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token", 400)
+
+    user.session_token = ""
+    user.session_expiration = datetime.datetime.now()
+    user.update_token = ""
+    db.session.commit()
+
+    return success_response({"message": "You have successfully logged out"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- src/db.py
+++ src/db.py
@@ -1,5 +1,10 @@
 import datetime
+import hashlib
+import os
+
+import bcrypt
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
@@ -17,6 +22,74 @@ association_table_journals_emotions = db.Table("association_e", db.Model.metadat
                                                      db.ForeignKey("emotion.id"))
                                            )
 
+class User(db.Model):
+    """
+    User model
+    """
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+
+    # User information
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_digest = db.Column(db.String, nullable=False)
+
+    # Entry information
+    entries = db.relationship("Entry", cascade="delete")
+
+    # Session information
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a User object
+        """
+        self.email = kwargs.get("email")
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
+
+    def get_user_entries(self):
+        return {
+            "entries": [e.serialize() for e in self.entries]
+        }
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        # Change expiration time accordingly
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(minutes=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        """
+        Verifies the password of a user
+        """
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        """
+        Verifies the session token of a user
+        """
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
+
 
 class Entry(db.Model):
     '''
@@ -28,6 +101,7 @@ class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     
     date = db.Column(db.String, nullable=True)
 
@@ -46,7 +120,7 @@ class Entry(db.Model):
         return {
             "title": self.title,
             "content": self.content,
-            "emotion": self.emotion,
+            "emotion": self.entry_emotions,
             "date": self.date
         }
 
